6b814e9613e8dda41cca4d6f17cf2ecb96ee8062
James-Eagelton <jameseagelton@gmail.com>
2024-10-11 16:14:03 +0100

    asdadasfcx
    asdfadf

--- Assets/Scripts/CameraMove.cs
+++ Assets/Scripts/CameraMove.cs
@@ -2,30 +2,57 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class CameraMove : MonoBehaviour
 {
     public Camera mainCamera;
     public GameObject viewPoint;
     public GameObject mapPoint;
-    public bool firstPerson;
-    
+    public float mouseSensitivity = 100f;
+    float xRotation = 0f;
+    float yRotation = 0f;
+
+    public bool freeRotation;
+
+ 
+
     void Update()
     {
-
-        if (Input.GetKeyDown(KeyCode.M)) 
+        if (Input.GetKeyDown(KeyCode.LeftAlt)) 
         {
-            firstPerson = !firstPerson;
+            freeRotation = !freeRotation;
+            
+        }
+
+        if (freeRotation) 
+        {
+            
+            Cursor.lockState = CursorLockMode.Locked;
+            float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+            float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+            xRotation -= mouseY;
+            yRotation += mouseX;
+            xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+            yRotation = Mathf.Clamp(yRotation, -90f, 90f);
+
+            mainCamera.transform.localRotation = Quaternion.Euler(xRotation, yRotation, 0f);
+            mainCamera.transform.Rotate(Vector3.up * mouseX);
+            mainCamera.transform.Rotate(Vector3.left * mouseY);
+
         }
         
-        if (firstPerson)
+        if (!freeRotation)
         {
+            Cursor.lockState = CursorLockMode.None;
             mainCamera.transform.position = viewPoint.transform.position;
             mainCamera.transform.rotation = viewPoint.transform.rotation;
         }
-        else 
-        {
-            mainCamera.transform.position = mapPoint.transform.position;
-            mainCamera.transform.rotation = mapPoint.transform.rotation;
-        }
+
+
+       
+
+
+       
     }
 }
